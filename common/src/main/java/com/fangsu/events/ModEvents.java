2f1e60c5cdc64bd686efa2103aba978727901a0b
596wjr <wjr596@qq.com>
2026-06-16 20:21:48 +0800

    修复服务端无法启动

--- common/src/main/java/com/fangsu/events/ModEvents.java
+++ common/src/main/java/com/fangsu/events/ModEvents.java
@@ -10,10 +10,5 @@ public class ModEvents {
     public static void init() {
         PlayerEvent.PLAYER_JOIN.register(JoinInMessage::sendJoinMessage);
         PlayerEvent.PLAYER_JOIN.register(JoinInClearCache::clearCache);
-
-        // 资源重载时清除缓存的绘制数据和路线缓存，防止跨世界数据错乱
-        if (Main.isClient) {
-            MainClient.addResourceRunnable(JoinInClearCache::clearCache);
-        }
     }
 }
