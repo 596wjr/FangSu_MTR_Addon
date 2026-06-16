60c66908372ca84155afee26517b7c5ef9e0c31d
596wjr <wjr596@qq.com>
2026-06-16 20:25:30 +0800

    修复服务端无法启动

--- common/src/main/java/com/fangsu/events/ModEvents.java
+++ common/src/main/java/com/fangsu/events/ModEvents.java
@@ -1,9 +1,5 @@
 package com.fangsu.events;
 
-import com.fangsu.Main;
-import com.fangsu.MainClient;
-import com.fangsu.mtr.DrawableRoute;
-import com.fangsu.utils.GraphicsTextureHelper;
 import dev.architectury.event.events.common.PlayerEvent;
 
 public class ModEvents {
