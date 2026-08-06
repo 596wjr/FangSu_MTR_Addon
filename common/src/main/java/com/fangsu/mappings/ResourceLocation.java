ae9db6d1dafc227be2cd1005d6db22fc8dc7b477
596wjr <116290244+596wjr@users.noreply.github.com>
2026-08-06 21:07:28 +0800

    try: 重写mappings系统

--- common/src/main/java/com/fangsu/mappings/ResourceLocation.java
+++ common/src/main/java/com/fangsu/mappings/ResourceLocation.java
@@ -1,23 +1,23 @@
 package com.fangsu.mappings;
 
-public class ResourceLocation {
-    private final net.minecraft.resources.ResourceLocation raw;
+public class ResourceLocation extends SimpleMappings<net.minecraft.resources.ResourceLocation>{
+    // private final net.minecraft.resources.ResourceLocation raw;
 
-    public ResourceLocation(net.minecraft.resources.ResourceLocation raw) {
-        this.raw = raw;
-    }
+    // public ResourceLocation(net.minecraft.resources.ResourceLocation raw) {
+    //     this.raw = raw;
+    // }
 
     public ResourceLocation(String path) {
-        this.raw = new net.minecraft.resources.ResourceLocation(path);
+        super(new net.minecraft.resources.ResourceLocation(path));
     }
 
     public ResourceLocation(String str1, String str2) {
-        this.raw = new net.minecraft.resources.ResourceLocation(str1, str2);
+        super( new net.minecraft.resources.ResourceLocation(str1, str2));
     }
 
-    public net.minecraft.resources.ResourceLocation getRaw() {
-        return raw;
-    }
+    // public net.minecraft.resources.ResourceLocation getRaw() {
+    //     return raw;
+    // }
 
     public String getPath() {
         return raw.getPath();
